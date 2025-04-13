b0774ba43b39540f54ea278d22a149b0c04354bf
socury <socury1104@gmail.com>
2025-04-14 08:42:44 +0900

    base

---
+++ src/main/java/com/pangapiserver/PangApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.pangapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PangApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pangapiserver/PangApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pangapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PangApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

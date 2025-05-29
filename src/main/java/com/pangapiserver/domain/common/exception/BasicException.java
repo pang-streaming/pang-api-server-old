f2ef27cd8d7e1edd85459e3ff9b045592ecf6658
0x1919 <79688235+Ingrsso@users.noreply.github.com>
2025-05-29 11:24:48 +0900

    Refer: Reflect review

--- src/main/java/com/pangapiserver/domain/common/exception/BasicException.java
+++ src/main/java/com/pangapiserver/domain/common/exception/BasicException.java
@@ -1,8 +1,10 @@
 package com.pangapiserver.domain.common.exception;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Getter
 public class BasicException extends RuntimeException {
     private final StatusCode statusCode;
 
--- src/main/java/com/pangapiserver/infrastructure/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pangapiserver/infrastructure/common/exception/GlobalExceptionHandler.java
@@ -14,17 +14,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(BasicException.class)
     private ResponseEntity<BaseResponse> handleBasicException(BasicException e) {
-        StatusCode statusCode = (StatusCode) e;
+        StatusCode statusCode = e.getStatusCode();
         BaseResponse response = BaseResponse.error(
                 statusCode.getHttpStatus(),
                 statusCode.getMessage()
         );
-        return new ResponseEntity<>(response, statusCode.getHttpStatus());
+        return ResponseEntity.status(statusCode.getHttpStatus()).body(response);
     }
 
     @ExceptionHandler(Exception.class)
     private ResponseEntity<BaseResponse> handleAll(Exception e) {
-        BaseResponse response = BaseResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-        return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        BaseResponse response = BaseResponse.error(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                e.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 }

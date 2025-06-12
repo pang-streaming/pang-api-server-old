9e2358a65820cdfa72e091e2af8b1e91d2219964
socury <socury1104@gmail.com>
2025-06-12 19:42:25 +0900

    refactor: clean up unused imports in GlobalExceptionHandler

--- src/main/java/com/pangapiserver/infrastructure/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pangapiserver/infrastructure/common/exception/GlobalExceptionHandler.java
@@ -1,13 +1,10 @@
 package com.pangapiserver.infrastructure.common.exception;
 
-import com.pangapiserver.domain.common.exception.BasicException;
-import com.pangapiserver.domain.common.exception.StatusCode;
-import com.pangapiserver.infrastructure.common.dto.DataResponse;
-import com.pangapiserver.infrastructure.common.dto.ErrorResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import com.pangapiserver.domain.common.exception.BasicException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.pangapiserver.infrastructure.common.dto.ErrorResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice

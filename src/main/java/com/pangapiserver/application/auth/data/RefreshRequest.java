59c1f2ec0b7464fc097b7d79a2b60bf7956cb50e
socury <socury1104@gmail.com>
2025-06-19 08:14:49 +0900

    refactor: add @NotBlank validation to RefreshRequest

--- src/main/java/com/pangapiserver/application/auth/data/RefreshRequest.java
+++ src/main/java/com/pangapiserver/application/auth/data/RefreshRequest.java
@@ -1,6 +1,9 @@
 package com.pangapiserver.application.auth.data;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record RefreshRequest(
-    String refreshToken
+        @NotBlank
+        String refreshToken
 ) {
 }

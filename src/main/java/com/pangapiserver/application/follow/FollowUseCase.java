1eaaff20cef373be428434b4ea2f740f2c72e1ff
김민준 <kmjons01@gmail.com>
2025-06-18 16:13:39 +0900

    refactor: apply review

--- src/main/java/com/pangapiserver/application/follow/FollowUseCase.java
+++ src/main/java/com/pangapiserver/application/follow/FollowUseCase.java
@@ -1,12 +1,10 @@
 package com.pangapiserver.application.follow;
 
 import java.util.List;
-
-import com.pangapiserver.infrastructure.common.dto.Response;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import com.pangapiserver.domain.user.entity.UserEntity;
+import com.pangapiserver.infrastructure.common.dto.Response;
 import com.pangapiserver.domain.follow.service.FollowService;
 import com.pangapiserver.infrastructure.common.dto.DataResponse;
 import com.pangapiserver.application.follow.data.FollowingResponse;
@@ -19,23 +17,26 @@ public class FollowUseCase {
     private final UserAuthenticationHolder userAuthHolder;
 
     public DataResponse<List<FollowingResponse>> getFollowings(String username) {
-        return DataResponse.ok(service.getByFollowing(isNull(username)));
+        return DataResponse.ok(service.getByFollowing(
+                        username == null
+                                ? userAuthHolder.current().getUsername()
+                                : username
+                )
+        );
     }
 
     public DataResponse<List<FollowingResponse>> getFollowers(String username) {
-        return DataResponse.ok(service.getByFollower(isNull(username)));
+        return DataResponse.ok(service.getByFollower(
+            username == null
+                ? userAuthHolder.current().getUsername()
+                : username
+            )
+        );
     }
 
     public Response followOrUnfollow(String username) {
         UserEntity user = userAuthHolder.current();
         service.followOrNot(user, username);
         return Response.ok("success");
-    }
-
-    private String isNull(String username) {
-        if (username == null) {
-            username = userAuthHolder.current().getUsername();
-        }
-        return username;
     }
 }

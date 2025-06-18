8d11c18036d4e9da861e742ede0a60bfe863384f
김민준 <kmjons01@gmail.com>
2025-06-18 15:21:21 +0900

    refactor: change response

--- src/main/java/com/pangapiserver/application/follow/FollowUseCase.java
+++ src/main/java/com/pangapiserver/application/follow/FollowUseCase.java
@@ -1,12 +1,14 @@
 package com.pangapiserver.application.follow;
 
 import java.util.List;
+
+import com.pangapiserver.infrastructure.common.dto.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import com.pangapiserver.domain.user.entity.UserEntity;
 import com.pangapiserver.domain.follow.service.FollowService;
-import com.pangapiserver.infrastructure.common.dto.BaseResponse;
+import com.pangapiserver.infrastructure.common.dto.DataResponse;
 import com.pangapiserver.application.follow.data.FollowingResponse;
 import com.pangapiserver.infrastructure.security.support.UserAuthenticationHolder;
 
@@ -16,18 +18,18 @@ public class FollowUseCase {
     private final FollowService service;
     private final UserAuthenticationHolder userAuthHolder;
 
-    public BaseResponse<List<FollowingResponse>> getFollowings(String username) {
-        return BaseResponse.ok(HttpStatus.OK, service.getByFollowing(isNull(username)));
+    public DataResponse<List<FollowingResponse>> getFollowings(String username) {
+        return DataResponse.ok(service.getByFollowing(isNull(username)));
     }
 
-    public BaseResponse<List<FollowingResponse>> getFollowers(String username) {
-        return BaseResponse.ok(HttpStatus.OK, service.getByFollower(isNull(username)));
+    public DataResponse<List<FollowingResponse>> getFollowers(String username) {
+        return DataResponse.ok(service.getByFollower(isNull(username)));
     }
 
-    public BaseResponse<?> followOrUnfollow(String username) {
+    public Response followOrUnfollow(String username) {
         UserEntity user = userAuthHolder.current();
         service.followOrNot(user, username);
-        return BaseResponse.ok(HttpStatus.OK, null);
+        return Response.ok("success");
     }
 
     private String isNull(String username) {
--- src/main/java/com/pangapiserver/presentation/follow/FollowController.java
+++ src/main/java/com/pangapiserver/presentation/follow/FollowController.java
@@ -1,10 +1,12 @@
 package com.pangapiserver.presentation.follow;
 
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import com.pangapiserver.application.follow.FollowUseCase;
-import com.pangapiserver.infrastructure.common.dto.BaseResponse;
+import com.pangapiserver.infrastructure.common.dto.Response;
+import com.pangapiserver.infrastructure.common.dto.DataResponse;
 import com.pangapiserver.application.follow.data.FollowingResponse;
 
 @RestController
@@ -14,17 +16,17 @@ public class FollowController {
     private final FollowUseCase useCase;
 
     @GetMapping("/following")
-    public BaseResponse<List<FollowingResponse>> getFollowings(@RequestParam(name = "username", required = false) String username) {
+    public DataResponse<List<FollowingResponse>> getFollowings(@RequestParam(name = "username", required = false) String username) {
         return useCase.getFollowings(username);
     }
 
     @GetMapping("/follower")
-    public BaseResponse<List<FollowingResponse>> getFollowers(@RequestParam(name = "username", required = false) String username) {
+    public DataResponse<List<FollowingResponse>> getFollowers(@RequestParam(name = "username", required = false) String username) {
         return useCase.getFollowers(username);
     }
 
     @PostMapping
-    public BaseResponse<?> followOrUnfollow(@RequestParam("username") String username) {
+    public Response followOrUnfollow(@RequestParam("username") String username) {
         return useCase.followOrUnfollow(username);
     }
 }

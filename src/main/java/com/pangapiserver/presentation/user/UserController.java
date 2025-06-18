d52450363158f80877cb891240325957f675e3f3
socury <socury1104@gmail.com>
2025-06-18 20:45:51 +0900

    feat: add endpoint to fetch current user info via `GET /me`

--- src/main/java/com/pangapiserver/presentation/user/UserController.java
+++ src/main/java/com/pangapiserver/presentation/user/UserController.java
@@ -2,7 +2,10 @@ package com.pangapiserver.presentation.user;
 
 import com.pangapiserver.application.user.UserUseCase;
 import com.pangapiserver.application.user.data.UpdateInfoRequest;
+import com.pangapiserver.application.user.data.UserInfoResponse;
+import com.pangapiserver.infrastructure.common.dto.DataResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserUseCase useCase;
+
+    @GetMapping("/me")
+    public DataResponse<UserInfoResponse> getMyInfo() {
+        return useCase.getMyInfo();
+    }
 
     @PatchMapping
     public void updateUser(UpdateInfoRequest request) {

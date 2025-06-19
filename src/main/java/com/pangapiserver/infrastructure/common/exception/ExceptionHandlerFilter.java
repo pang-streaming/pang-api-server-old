cf01275bc2aa7657326a9766d87c65228b1d6a86
김민준 <kmjons01@gmail.com>
2025-06-19 12:21:34 +0900

    chore: remove unused code

--- src/main/java/com/pangapiserver/infrastructure/common/exception/ExceptionHandlerFilter.java
+++ src/main/java/com/pangapiserver/infrastructure/common/exception/ExceptionHandlerFilter.java
@@ -1,7 +1,6 @@
 package com.pangapiserver.infrastructure.common.exception;
 
 import java.io.IOException;
-import lombok.extern.slf4j.Slf4j;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;

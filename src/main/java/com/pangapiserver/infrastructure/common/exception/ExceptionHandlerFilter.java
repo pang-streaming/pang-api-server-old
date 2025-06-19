6f34dd0991c515b8626b07f7772cebf4714c061d
김민준 <kmjons01@gmail.com>
2025-06-19 16:55:17 +0900

    chore: apply review

--- src/main/java/com/pangapiserver/infrastructure/common/exception/ExceptionHandlerFilter.java
+++ src/main/java/com/pangapiserver/infrastructure/common/exception/ExceptionHandlerFilter.java
@@ -12,6 +12,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import com.pangapiserver.infrastructure.common.dto.Response;
 import com.pangapiserver.domain.common.exception.StatusCode;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.pangapiserver.domain.common.exception.BasicException;
 import com.pangapiserver.infrastructure.common.dto.ErrorResponse;
 import com.pangapiserver.domain.common.exception.GlobalExceptionStatusCode;
 
@@ -22,7 +23,7 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
         throws ServletException, IOException {
         try {
             filterChain.doFilter(request, response);
-        } catch (Exception e) {
+        } catch (BasicException e) {
             setErrorResponse(response, GlobalExceptionStatusCode.INTERNAL_SERVER_ERROR);
         }
     }
@@ -31,15 +32,12 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
         response.setStatus(errorCode.getHttpStatus().value());
         response.setContentType("application/json; charset=UTF-8");
 
-        Response errorResponse = ErrorResponse.builder()
-            .status(errorCode.getHttpStatus())
-            .message(errorCode.getMessage())
-            .build();
+        Response errorResponse = ErrorResponse.responseEntity(errorCode).getBody();
 
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        ObjectMapper mapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .enable(SerializationFeature.INDENT_OUTPUT);
 
         String jsonResponse = mapper.writeValueAsString(errorResponse);
         response.getWriter()

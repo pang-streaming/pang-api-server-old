b083cd1a4755175cb2d3692fc4861c709b3732eb
Ingrsso <79688235+Ingrsso@users.noreply.github.com>
2025-07-11 10:58:34 +0900

    Refactor: 함수명 좀더 명확하게 수정

--- src/main/java/com/pangapiserver/domain/cash/service/CashService.java
+++ src/main/java/com/pangapiserver/domain/cash/service/CashService.java
@@ -4,11 +4,8 @@ import com.pangapiserver.domain.cash.data.CashTransactionDto;
 import com.pangapiserver.domain.cash.entity.CashEntity;
 import com.pangapiserver.domain.cash.enumeration.CashType;
 import com.pangapiserver.domain.cash.exception.InsufficientBalanceException;
-import com.pangapiserver.domain.cash.repository.CashQueryRepository;
-import com.pangapiserver.domain.cash.repository.CashQueryRepositoryImpl;
 import com.pangapiserver.domain.cash.repository.CashRepository;
 import com.pangapiserver.domain.user.entity.UserEntity;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,9 +15,9 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CashService {
-    private final CashRepository cashRepository;
+    private final CashRepository repository;
 
-    public void charge(UserEntity user, int amount, String description) {
+    public void deposit(UserEntity user, int amount, String description) {
         CashEntity cash = CashEntity.builder()
             .user(user)
             .type(CashType.CHARGE)
@@ -29,10 +26,10 @@ public class CashService {
             .created_at(LocalDateTime.now())
             .build();
 
-        cashRepository.save(cash);
+        repository.save(cash);
     }
 
-    public void use(UserEntity user, int amount, String description) {
+    public void withdraw(UserEntity user, int amount, String description) {
         int balance = getBalance(user);
         if (balance < amount) {
             throw new InsufficientBalanceException();
@@ -46,7 +43,7 @@ public class CashService {
             .created_at(LocalDateTime.now())
             .build();
 
-        cashRepository.save(cash);
+        repository.save(cash);
     }
 
     public void refund(UserEntity user, int amount, String description) {
@@ -58,14 +55,14 @@ public class CashService {
             .created_at(LocalDateTime.now())
             .build();
 
-        cashRepository.save(cash);
+        repository.save(cash);
     }
 
     public int getBalance(UserEntity user) {
-        return cashRepository.sumAmountByUserId(user.getId()).orElse(0);
+        return repository.sumAmountByUserId(user.getId()).orElse(0);
     }
 
     public List<CashTransactionDto> getTransactions(UserEntity user) {
-        return cashRepository.findAllByUser(user);
+        return repository.findAllByUser(user);
     }
 }

7e5568aa83a5fc90155cf62f92f9de3aa7e89cd9
Ingrsso <79688235+Ingrsso@users.noreply.github.com>
2025-07-11 12:43:07 +0900

    Refactor: Exception 오버로딩 처리 로 동적 예외 반환

--- src/main/java/com/pangapiserver/infrastructure/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pangapiserver/infrastructure/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.pangapiserver.infrastructure.common.exception;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import com.pangapiserver.domain.common.exception.BasicException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,14 +10,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
+@Log4j2
 public class GlobalExceptionHandler {
     @ExceptionHandler(BasicException.class)
     public ResponseEntity<ErrorResponse> basicExceptionHandler(BasicException e) {
-        return ErrorResponse.responseEntity(e.getStatusCode());
+        return ErrorResponse.responseEntity(e);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleAll(Exception e) {
+        log.error("Exception Occurred", e);
         return ErrorResponse.responseEntity(GlobalStatusCode.INTERNAL_SERVER_ERROR);
     }
 }

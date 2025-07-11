077c8b6384ec3e17282e64bfb427a6aec2b41f3a
Ingrsso <79688235+Ingrsso@users.noreply.github.com>
2025-07-11 11:00:58 +0900

    Chore: @RequiredArgsConstructor를 @AllArgsConstructor로 변경

--- src/main/java/com/pangapiserver/domain/cash/exception/CashExceptionStatusCode.java
+++ src/main/java/com/pangapiserver/domain/cash/exception/CashExceptionStatusCode.java
@@ -1,10 +1,11 @@
 package com.pangapiserver.domain.cash.exception;
 
 import com.pangapiserver.domain.common.exception.StatusCode;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@RequiredArgsConstructor
+@AllArgsConstructor
 public enum CashExceptionStatusCode implements StatusCode {
     INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST, "잔액이 부족합니다."),
 

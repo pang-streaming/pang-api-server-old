4ed232b0304443164999806e9b0a68cbe0d1dbdb
kmg <legolove08@naver.com>
2025-07-18 00:58:59 +0900

    chore: swagger server

--- src/main/java/com/pangapiserver/infrastructure/swagger/SwaggerConfig.java
+++ src/main/java/com/pangapiserver/infrastructure/swagger/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -22,6 +25,7 @@ public class SwaggerConfig {
         );
         return new OpenAPI()
                 .components(new Components())
+                .servers(List.of(new Server().url("https://pang-api.euns.dev")))
                 .info(apiInfo())
                 .addSecurityItem(securityRequirement)
                 .components(components);

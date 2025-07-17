d49fabb701bfa28ceb3db577938ffec605762a53
socury <socury1104@gmail.com>
2025-07-18 02:03:44 +0900

    fix: update allowed methods in CORS configuration to include PATCH

--- src/main/java/com/pangapiserver/infrastructure/security/configuration/SecurityConfig.java
+++ src/main/java/com/pangapiserver/infrastructure/security/configuration/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(false);
 

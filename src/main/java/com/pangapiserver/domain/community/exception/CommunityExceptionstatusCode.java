2e6037a698e67bec4dbf1821d61b96d1307996c6
socury <socury1104@gmail.com>
2025-09-10 21:55:09 +0900

    feat: add CommunityExceptionstatusCode and CommunityNotfoundException for improved error handling

---
+++ src/main/java/com/pangapiserver/domain/community/exception/CommunityExceptionstatusCode.java
@@ -0,0 +1,25 @@
+package com.pangapiserver.domain.community.exception;
+
+import com.pangapiserver.domain.common.exception.StatusCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum CommunityExceptionstatusCode implements StatusCode {
+    POST_NOTFOUND_EXCEPTION(HttpStatus.NOT_FOUND, "없는 게시물입니다."),
+    COMMUNITY_NOTFOUND_EXCEPTION(HttpStatus.NOT_FOUND, "없는 커뮤니티입니다."),
+
+    ;
+    private final HttpStatus status;
+    private final String message;
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return status;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}

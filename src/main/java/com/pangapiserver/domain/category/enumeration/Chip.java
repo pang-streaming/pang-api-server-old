62e0055d19b15a2ca593082bd0b135f626512b69
socury <socury1104@gmail.com>
2025-10-01 15:31:20 +0900

    feat: add Category

---
+++ src/main/java/com/pangapiserver/domain/category/enumeration/Chip.java
@@ -0,0 +1,16 @@
+package com.pangapiserver.domain.category.enumeration;
+
+public enum Chip {
+    GAME, // 게임
+    TRAVEL, // 여행
+    EATING_SHOW, // 먹방
+    VIRTUAL, // 버츄얼
+    SPORTS, // 스포츠
+    MUSIC, // 음악
+    DRAWING, // 그림
+    TALK, // 토크
+    COOKING, // 요리
+    CURRENT_AFFAIRS, // 시사
+    STUDY, // 공부
+    OTHERS
+}

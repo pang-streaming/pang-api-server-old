1ebaf7739a85af70651483fe51c94fae500957d8
socury <socury1104@gmail.com>
2025-10-16 19:48:46 +0900

    feat: add username field to FollowingResponse for enhanced user details

--- src/main/java/com/pangapiserver/application/follow/data/FollowingResponse.java
+++ src/main/java/com/pangapiserver/application/follow/data/FollowingResponse.java
@@ -4,12 +4,14 @@ import com.pangapiserver.domain.user.entity.UserEntity;
 
 public record FollowingResponse (
         String image,
+        String username,
         String nickname,
         Long follower
 ) {
     public static FollowingResponse of(UserEntity user, Long followerCount) {
         return new FollowingResponse(
                 user.getProfileImage(),
+                user.getUsername(),
                 user.getNickname(),
                 followerCount
         );

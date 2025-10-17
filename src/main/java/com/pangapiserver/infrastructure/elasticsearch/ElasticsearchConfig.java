5e418f7c2d935a140cdf4a482b31579b1b946e6e
socury <socury1104@gmail.com>
2025-10-18 05:18:35 +0900

    fix: remove unused SSL truststore configuration from Elasticsearch setup

--- src/main/java/com/pangapiserver/infrastructure/elasticsearch/ElasticsearchConfig.java
+++ src/main/java/com/pangapiserver/infrastructure/elasticsearch/ElasticsearchConfig.java
@@ -1,11 +1,8 @@
 package com.pangapiserver.infrastructure.elasticsearch;
 
-import com.pangapiserver.infrastructure.elasticsearch.exception.ElasticsearchConnectionException;
 import com.pangapiserver.infrastructure.elasticsearch.properties.ElasticsearchProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.apache.hc.client5.http.ssl.TrustAllStrategy;
-import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
@@ -18,16 +15,10 @@ public class ElasticsearchConfig extends ElasticsearchConfiguration {
 
     @Override
     public ClientConfiguration clientConfiguration() {
-        try {
-            return ClientConfiguration.builder()
-                    .connectedTo(properties.getUris())
-                    .usingSsl(SSLContextBuilder.create()
-                            .loadTrustMaterial(new TrustAllStrategy()).build())
-                    .withBasicAuth(properties.getUsername(), properties.getPassword())
-                    .build();
-        } catch (Exception e) {
-            log.error("Elasticsearch 연결에 실패했습니다. 메시지: {} 에러: {}", e.getMessage(), e);
-            throw new ElasticsearchConnectionException();
-        }
+        return ClientConfiguration.builder()
+                .connectedTo(properties.getUris())
+                .withBasicAuth(properties.getUsername(), properties.getPassword())
+                .build();
     }
+
 }

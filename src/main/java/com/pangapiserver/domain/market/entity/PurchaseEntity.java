6f494f57a706acdf5d2595eff79929762b64f515
socury <socury1104@gmail.com>
2025-10-18 11:28:33 +0900

    feat: add delivery status and address fields to purchases

--- src/main/java/com/pangapiserver/domain/market/entity/PurchaseEntity.java
+++ src/main/java/com/pangapiserver/domain/market/entity/PurchaseEntity.java
@@ -1,5 +1,6 @@
 package com.pangapiserver.domain.market.entity;
 
+import com.pangapiserver.domain.market.enumeration.DeliveryStatus;
 import com.pangapiserver.domain.user.entity.UserEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -29,6 +30,13 @@ public class PurchaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "fk_user_id", nullable = false)
     private UserEntity buyer;
+
+    private String address;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus deliveryStatus;
 
     @CreatedDate
     private LocalDateTime createdAt;
---
+++ src/main/java/com/pangapiserver/domain/market/enumeration/DeliveryStatus.java
@@ -0,0 +1,7 @@
+package com.pangapiserver.domain.market.enumeration;
+
+public enum DeliveryStatus {
+    PREPARING, // 상품 준비중
+    SHIPPING, // 배송중
+    DELIVERED // 배송 완료
+}

f4c1731282b3f61c715f91a81855c1f9220b94c5
socury <socury1104@gmail.com>
2025-10-20 21:22:16 +0900

    fix: update getByUser method to throw UserNotFoundException instead of StreamKeyNotFoundException

--- src/main/java/com/pangapiserver/domain/stream/service/StreamKeyService.java
+++ src/main/java/com/pangapiserver/domain/stream/service/StreamKeyService.java
@@ -4,6 +4,7 @@ import com.pangapiserver.domain.stream.entity.StreamKeyEntity;
 import com.pangapiserver.domain.stream.exception.StreamKeyNotFoundException;
 import com.pangapiserver.domain.stream.repository.StreamKeyRepository;
 import com.pangapiserver.domain.user.entity.UserEntity;
+import com.pangapiserver.domain.user.exception.UserNotFoundException;
 import com.pangapiserver.infrastructure.encode.Sha512Encoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,14 +17,14 @@ public class StreamKeyService {
     public StreamKeyEntity create(UserEntity user) {
         StreamKeyEntity streamKey = repository.findByUser(user)
             .orElse(StreamKeyEntity.create(user));
-        String key = Sha512Encoder.encode(user.getUsername());
+        String key = Sha512Encoder.encode(Sha512Encoder.encode(user.getUsername()));
         streamKey.updateKey(key);
         return repository.save(streamKey);
     }
 
     public StreamKeyEntity getByUser(UserEntity user) {
         return repository.findByUser(user)
-            .orElseThrow(StreamKeyNotFoundException::new);
+            .orElseThrow(UserNotFoundException::new);
     }
 
     public StreamKeyEntity getByStreamKey(String encodeKey) {

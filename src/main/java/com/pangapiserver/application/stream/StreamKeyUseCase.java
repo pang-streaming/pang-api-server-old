fff2d770a9dfcd1661b01c924e092c4c42d2f880
socury <socury1104@gmail.com>
2025-10-21 23:39:41 +0900

    refactor: add isStreaming endpoint and StreamStatusResponse for stream status checks

--- src/main/java/com/pangapiserver/application/stream/StreamKeyUseCase.java
+++ src/main/java/com/pangapiserver/application/stream/StreamKeyUseCase.java
@@ -1,10 +1,14 @@
 package com.pangapiserver.application.stream;
 
 import com.pangapiserver.application.stream.data.response.StreamKeyResponse;
+import com.pangapiserver.application.stream.data.response.StreamStatusResponse;
 import com.pangapiserver.domain.community.entity.CommunityEntity;
 import com.pangapiserver.domain.community.service.CommunityService;
+import com.pangapiserver.domain.stream.entity.StreamEntity;
+import com.pangapiserver.domain.stream.entity.StreamKeyEntity;
 import com.pangapiserver.domain.stream.entity.StreamType;
 import com.pangapiserver.domain.stream.service.StreamKeyService;
+import com.pangapiserver.domain.stream.service.StreamService;
 import com.pangapiserver.domain.user.entity.UserEntity;
 import com.pangapiserver.domain.user.enumeration.Role;
 import com.pangapiserver.domain.user.service.UserService;
@@ -19,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class StreamKeyUseCase {
     private final StreamKeyService service;
+    private final StreamService streamService;
     private final CommunityService communityService;
     private final UserService userService;
     private final UserAuthenticationHolder holder;
@@ -37,5 +42,13 @@ public class StreamKeyUseCase {
     public DataResponse<StreamKeyResponse> getKey() {
         UserEntity user = holder.current();
         return DataResponse.ok("스트리밍 키 조회 성공", StreamKeyResponse.of(service.getByUser(user)));
+    }
+
+    public DataResponse<StreamStatusResponse> isStreaming() {
+        UserEntity user = holder.current();
+        String key = service.getByUser(user);
+        StreamKeyEntity byStreamKey = service.getByStreamKey(key);
+        StreamEntity stream = streamService.getLiveStreamByUser(user);
+        return DataResponse.ok("스트리밍 상태 확인 성공", StreamStatusResponse.of(stream.getStatus(), byStreamKey.getType()));
     }
 }

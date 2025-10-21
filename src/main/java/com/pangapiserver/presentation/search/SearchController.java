a20f7b45a8dd594b2a3c39bbf3965423431b0704
Erezes <kmjons01@gmail.com>
2025-10-21 14:16:03 +0900

    feat: add search documents

--- src/main/java/com/pangapiserver/presentation/search/SearchController.java
+++ src/main/java/com/pangapiserver/presentation/search/SearchController.java
@@ -4,20 +4,23 @@ import com.pangapiserver.application.search.SearchUseCase;
 import com.pangapiserver.application.search.data.TotalSearchResponse;
 import com.pangapiserver.infrastructure.common.dto.DataResponse;
 import com.pangapiserver.infrastructure.common.dto.Response;
+import com.pangapiserver.presentation.search.document.SearchDocuments;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/search")
 @RequiredArgsConstructor
-public class SearchController {
+public class SearchController implements SearchDocuments {
     private final SearchUseCase useCase;
 
+    @Override
     @GetMapping("/{keyword}")
     public DataResponse<TotalSearchResponse> search(@PathVariable("keyword") String keyword) {
         return useCase.search(keyword);
     }
 
+    @Override
     @PostMapping("/reindex")
     public Response reindex() {
         return useCase.reindex();

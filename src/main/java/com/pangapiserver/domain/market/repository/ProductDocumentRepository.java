ea6b52a708fb7a422afb624b06e8fd1874b4ff89
Erezes <kmjons01@gmail.com>
2025-10-21 09:16:36 +0900

    fix: change product document repository to elasticsearch repository

--- src/main/java/com/pangapiserver/domain/market/repository/ProductDocumentRepository.java
+++ src/main/java/com/pangapiserver/domain/market/repository/ProductDocumentRepository.java
@@ -1,9 +1,11 @@
 package com.pangapiserver.domain.market.repository;
 
 import com.pangapiserver.domain.market.document.ProductDocument;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-public interface ProductDocumentRepository extends JpaRepository<ProductDocument, UUID> {
+@Repository
+public interface ProductDocumentRepository extends ElasticsearchRepository<ProductDocument, UUID> {
 }
